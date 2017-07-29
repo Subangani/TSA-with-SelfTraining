b5d8d7624d7f8501ab26a047429bbe0e31385e19
Subangani <subangani1993@gmail.com>
2017-07-29 20:59:17 +0530

    update

--- src/svm.py
+++ src/svm.py
@@ -10,6 +10,9 @@ import tune
 positiveFile="../dataset/posTrain.csv"
 negativeFile="../dataset/negTrain.csv"
 neutralFile="../dataset/neuTrain.csv"
+positiveTuneFile="../dataset/posSelf.csv"
+negativeTuneFile="../dataset/negSelf.csv"
+neutralTuneFile="../dataset/neuSelf.csv"
 MODEL=null
 scaler=null
 normalizer=null
@@ -37,6 +40,19 @@ def svm_Model():
     X = X.tolist()
 
     KERNEL_FUNCTION,C_PARAMETER= tune.get_Kernel_Cparameter(X, Y)
+    #
+    # X_model, Y_model = generateVector.loadMatrix(positiveTuneFile, neutralTuneFile, negativeTuneFile, '2', '0', '-2')
+    #
+    # # features standardization
+    # X_scaled = pr.scale(np.array(X_model))
+    # scaler = pr.StandardScaler().fit(X_model)  # to use later for testing data scaler.transform(X)
+    #
+    # # features Normalization
+    # X_normalized = pr.normalize(X_scaled, norm='l2')  # l2 norm
+    # normalizer = pr.Normalizer().fit(X_scaled)  # as before normalizer.transform([[-1.,  1., 0.]]) for test
+    #
+    # X_model = X_normalized
+    # X_model = X_model.tolist()
 
     print "Training model with optimized parameters"
     MODEL = train_SVM_model(X, Y, KERNEL_FUNCTION, C_PARAMETER)
@@ -72,68 +88,74 @@ def test(model):
     getAccuracyPrecision()
 
 def getAccuracyPrecision():
-    testedFile="../dataset/test.csv.svm_result"
-    f0=open(testedFile,"r")
-    line = f0.readline() #pass first line
-    TP=0
-    TN=0
-    TNeu=0
-    FP=0
-    FN=0
-    FNeu=0
-    NeuTr=0
-    NegTr=0
-    TrNeg=0
-    NeuNeg=0
-    TrNeu=0
-    NegNeu=0
+    try:
+        testedFile="../dataset/test.csv.svm_result"
+        f0=open(testedFile,"r")
+        line = f0.readline() #pass first line
+        TP=0
+        TN=0
+        TNeu=0
+        FP=0
+        FN=0
+        FNeu=0
+        NeuTr=0
+        NegTr=0
+        TrNeg=0
+        NeuNeg=0
+        TrNeu=0
+        NegNeu=0
 
-    readers = csv.reader(f0)
-    for tweetResult in readers:
-        if ((tweetResult[0]=="positive") & (str(tweetResult[2])=="2.0")):
-            TP+=1
-        elif ((tweetResult[0]=="negative") & (tweetResult[2]=="-2.0")):
-            TN+=1
-        elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="0.0")):
-            TNeu+=1
-        elif (((tweetResult[0]=="negative") | (tweetResult[0]=="neutral")) & (tweetResult[2]=="2.0")):
-            FP+=1
-        elif (((tweetResult[0]=="positive")| (tweetResult[0]=="neutral")) & (tweetResult[2]=="-2.0")):
-            FN+=1
-        elif (((tweetResult[0]=="positive")| (tweetResult[0]=="negative")) & (tweetResult[2]=="0.0")):
-            FNeu+=1
+        readers = csv.reader(f0)
+        for tweetResult in readers:
+            if ((tweetResult[0]=="positive") & (str(tweetResult[2])=="2.0")):
+                TP+=1
+            elif ((tweetResult[0]=="negative") & (tweetResult[2]=="-2.0")):
+                TN+=1
+            elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="0.0")):
+                TNeu+=1
+            elif (((tweetResult[0]=="negative") | (tweetResult[0]=="neutral")) & (tweetResult[2]=="2.0")):
+                FP+=1
+            elif (((tweetResult[0]=="positive")| (tweetResult[0]=="neutral")) & (tweetResult[2]=="-2.0")):
+                FN+=1
+            elif (((tweetResult[0]=="positive")| (tweetResult[0]=="negative")) & (tweetResult[2]=="0.0")):
+                FNeu+=1
 
-        if ((tweetResult[0]=="positive") & (tweetResult[2]=="0.0")):
-            NeuTr+=1
-        elif ((tweetResult[0]=="positive") & (tweetResult[2]=="-2.0")):
-            NegTr+=1
-        elif ((tweetResult[0]=="negative") & (tweetResult[2]=="0.0")):
-            NeuNeg+=1
-        elif ((tweetResult[0]=="negative") & (tweetResult[2]=="2.0")):
-            TrNeg+=1
-        elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="2.0")):
-            TrNeu+=1
-        elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="-2.0")):
-            NegNeu+=1
-    print "TP,TN,TNeu,FP,FN,FNeu,NeuTr,NegTr,NeuNeg,TrNeg,TrNeu,NegNeu"
-    print TP,TN,TNeu,FP,FN,FNeu,NeuTr,NegTr,NeuNeg,TrNeg,TrNeu,NegNeu
-    acc=(TP+TN+TNeu)/((TP+TN+TNeu+FP+FN+FNeu)*1.0)
-    precision_pos=(TP/((FP+TP)*1.0))
-    precision_neg=(TN/((FN+TN)*1.0))
-    precision_neu=(TNeu/((FNeu+TNeu)*1.0))
+            if ((tweetResult[0]=="positive") & (tweetResult[2]=="0.0")):
+                NeuTr+=1
+            elif ((tweetResult[0]=="positive") & (tweetResult[2]=="-2.0")):
+                NegTr+=1
+            elif ((tweetResult[0]=="negative") & (tweetResult[2]=="0.0")):
+                NeuNeg+=1
+            elif ((tweetResult[0]=="negative") & (tweetResult[2]=="2.0")):
+                TrNeg+=1
+            elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="2.0")):
+                TrNeu+=1
+            elif ((tweetResult[0]=="neutral") & (tweetResult[2]=="-2.0")):
+                NegNeu+=1
+        print "TP,TN,TNeu,FP,FN,FNeu,NeuTr,NegTr,NeuNeg,TrNeg,TrNeu,NegNeu"
+        print TP,TN,TNeu,FP,FN,FNeu,NeuTr,NegTr,NeuNeg,TrNeg,TrNeu,NegNeu
+        acc=(TP+TN+TNeu)/((TP+TN+TNeu+FP+FN+FNeu)*1.0)
+        precision_pos=(TP/((FP+TP)*1.0))
+        precision_neg=(TN/((FN+TN)*1.0))
+        precision_neu=(TNeu/((FNeu+TNeu)*1.0))
 
-    recall_pos=TP/((TP+NegTr+NeuTr)*1.0)
-    recall_neg=TN/((TN+TrNeg+NeuNeg)*1.0)
-    recall_neu=TNeu/((TNeu+TrNeu+NegNeu)*1.0)
+        recall_pos=TP/((TP+NegTr+NeuTr)*1.0)
+        recall_neg=TN/((TN+TrNeg+NeuNeg)*1.0)
+        recall_neu=TNeu/((TNeu+TrNeu+NegNeu)*1.0)
 
-    F_core_pos=2*(precision_pos*recall_pos)/(precision_pos+recall_pos)
-    F_core_neg=2*(precision_neg*recall_neg)/(precision_neg+recall_neg)
-    F_core_neu=2*(precision_neu*recall_neu)/(precision_neu+recall_neu)
+        F_core_pos=2*(precision_pos*recall_pos)/(precision_pos+recall_pos)
+        F_core_neg=2*(precision_neg*recall_neg)/(precision_neg+recall_neg)
+        F_core_neu=2*(precision_neu*recall_neu)/(precision_neu+recall_neu)
 
-    print "acc,precision_pos,precision_neg,precision_neu,recall_pos,recall_neg,recall_neu"
-    print acc,precision_pos,precision_neg,precision_neu,recall_pos,recall_neg,recall_neu
-    print "F_core_pos,F_core_neg,F_core_neu"
-    print F_core_pos,F_core_neg,F_core_neu
+        print "acc,precision_pos,precision_neg,precision_neu,recall_pos,recall_neg,recall_neu"
+        print acc,precision_pos,precision_neg,precision_neu,recall_pos,recall_neg,recall_neu
+        print "F_core_pos,F_core_neg,F_core_neu"
+        print F_core_pos,F_core_neg,F_core_neu
+    except:
+        TypeError
+
+
+
 
 svm_Model()
 test(MODEL)
